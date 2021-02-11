e2d79f4d4fbc69c359802a8f885e40da1bf0190c
Hagino T <marspeoplehg@gmail.com>
2021-02-12 02:27:31 +0900

    設定の変更 rails sを自分でできるように

--- docker/rails/entrypoint.sh
+++ docker/rails/entrypoint.sh
@@ -11,7 +11,7 @@ rm -f /myapp/tmp/pids/server.pid
 mkdir -p tmp/pids
 mkdir -p tmp/sockets
 
-bundle exec puma -C config/puma.rb
-# rails s -p 3000 -b '0.0.0.0'
+# bundle exec puma -C config/puma.rb
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
